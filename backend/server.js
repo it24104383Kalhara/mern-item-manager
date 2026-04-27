1c0eda03369a9587103395d7502221b9545c99d3
IT24104383 Kalhara K.V <vinodkalhara906@gmail.com>
2026-04-27 07:03:40 +0530

    chore: updated the CORS

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const itemRoutes = require('./routes/items.js');
 const app = express();
 
 app.use(cors({
-    origin: ['https://wmt-mern-item-manager.netlify.app', 'http://localhost:5000']
+    origin: ["https://netlify.app", "http://localhost:5000"]
 }));
 app.use(express.json());
 
